0b579c30d4bae06e2a547a88023e23f0b9896399
Yohannes Alemu <you@example.com>
2024-01-03 13:29:10 -0800

    j

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range (0,100):
     if i <=9:
-        print("0{i}, ")
+        print(f"0{i}, ",end='')
     else:
-        print("{i}, ")
+        print(f"{i}, ",end='')
 
