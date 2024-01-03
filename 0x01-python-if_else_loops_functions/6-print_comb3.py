41b2ddb5ee7b1b6408888c433ca71f88beb2c816
Yohannes Alemu <you@example.com>
2024-01-03 14:54:43 -0800

    j

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,10 @@
 for i in range(0, 100):
     last_digit = i % 10
     front_digit = i//10
+    if i==89:
+        print("{:2}".format(i))
     if front_digit < last_digit:
         print("{:2}".format(i), end=", ")
     elif front_digit == last_digit:
         continue
+
