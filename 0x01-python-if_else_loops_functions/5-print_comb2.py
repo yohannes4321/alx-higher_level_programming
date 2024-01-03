d849b650aa00c69580804a30ab4d471bef4d92da
Yohannes Alemu <you@example.com>
2024-01-03 14:14:49 -0800

    j

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i == 99:
-        print("{i}".format(i) )
+        print("{}".format(i))
 
     else:
-        print("{:01}".format(i), end=', ')
+        print("{:02}".format(i), end=', ')
