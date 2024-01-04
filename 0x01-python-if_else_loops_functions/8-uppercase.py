2c8031718e9176758e59a478a4ad0559bb932416
Yohannes Alemu <you@example.com>
2024-01-04 05:06:52 -0800

    8-uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
-#!/bin/usr/python3
-
-for i in str:
-    str="deter"
-    print(i)
-
-
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(str):
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            letter = chr(ord(i) - 32)
+        print("{:s}".format(letter), end="")
