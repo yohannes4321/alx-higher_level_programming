4bcbf060757711497d71c720f7288a8bf0bc159b
Yohannes Alemu <you@example.com>
2024-01-03 16:05:14 -0800

    j

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    last_digit = i % 10
-    front_digit = i//10
-    if i==89:
-        print("{:2}".format(i))
-    if front_digit < last_digit && 1 != 89:
-        print("{:2}".format(i), end=", ")
-    elif front_digit == last_digit:
-        continue
-
+for i in range (0,10):
+    for j in range (i+1,10):
+        if i==8 and j==9:
+            print("{}{}".format(i,j))
+        else:
+        print("{}{}".format(i,j) ,end= ", ")
