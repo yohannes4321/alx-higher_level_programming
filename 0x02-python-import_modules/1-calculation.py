a2540fabf22e610d504fbecfdb1b3f9cd2284619
Yohannes Alemu <you@example.com>
2024-01-05 01:51:28 -0800

    1-calulation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
-# 1-calculation.py
 
-if __name__ == "__main__":
-    """Print the sum, difference, multiple and quotient of 10 and 5."""
-    from calculator_1 import add, sub, mul, div
+# Importing Functions from calculator_1 Module
+from calculator_1 import add
+from calculator_1 import sub
+from calculator_1 import mul
+from calculator_1 import div
 
+# Main Function Definition
+def main():
+    # Initializing Variables
     a = 10
     b = 5
-
+    
+    # Performing Addition
     print("{} + {} = {}".format(a, b, add(a, b)))
+
+    # Performing Subtraction
     print("{} - {} = {}".format(a, b, sub(a, b)))
+
+    # Performing Multiplication
     print("{} * {} = {}".format(a, b, mul(a, b)))
+
+    # Performing Division
     print("{} / {} = {}".format(a, b, div(a, b)))
+
+# Calling the Main Function
+main()
+
