7cce85eda36759e92f582e344e45014e296931d3
Yohannes Alemu <you@example.com>
2024-01-05 01:22:31 -0800

    hello Holysprit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-# 0_add.py
 
-if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
+if __name__ == '__main__':
     from add_0 import add
 
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    def main():
+        a = 1
+        b = 2
+        result = add(a, b)
+        print("{} + {} = {}".format(a, b, result))
+    main()
