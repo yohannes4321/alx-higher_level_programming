2b662fb0ae024eeccae7989118faad1af6afd343
Yohannes Alemu <you@example.com>
2024-01-05 01:57:55 -0800

    God is good

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-
+if __name__ == '__main__':
 # Importing Functions from calculator_1 Module
-from calculator_1 import add
-from calculator_1 import sub
-from calculator_1 import mul
-from calculator_1 import div
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import mul
+    from calculator_1 import div
 
 # Main Function Definition
-def main():
+    def main():
     # Initializing Variables
-    a = 10
-    b = 5
+        a = 10
+        b = 5
     
     # Performing Addition
-    print("{} + {} = {}".format(a, b, add(a, b)))
+        print("{} + {} = {}".format(a, b, add(a, b)))
 
     # Performing Subtraction
-    print("{} - {} = {}".format(a, b, sub(a, b)))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
 
     # Performing Multiplication
-    print("{} * {} = {}".format(a, b, mul(a, b)))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
 
     # Performing Division
-    print("{} / {} = {}".format(a, b, div(a, b)))
+        print("{} / {} = {}".format(a, b, div(a, b)))
 
 # Calling the Main Function
-main()
+    main()
 
