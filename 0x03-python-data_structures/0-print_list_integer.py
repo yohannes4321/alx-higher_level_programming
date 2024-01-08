adfc663c44860b67f9e36b45d31fa5487feac261
Yohannes Alemu <you@example.com>
2024-01-08 00:33:07 -0800

    0-print_list_intgers.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
-#!/bin/usr/python3
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+#!/usr/bin/python3
+
+def print_list_integer(my_list=None):
+    for number in range(len(my_list)):
+        print("{:d}".format(number))
