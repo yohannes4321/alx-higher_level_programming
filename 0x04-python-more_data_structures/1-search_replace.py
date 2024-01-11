41b11f646a6f31330fc60692ed33421f5e7bab33
Yohannes Alemu <you@example.com>
2024-01-11 12:05:03 -0800

    add new search and replace element

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == seach:
+            new_list[i] = repalce
+    return new_list
