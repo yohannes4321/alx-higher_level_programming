1b00118d8980119123ebe3cbc8b5732e020c266d
Yohannes Alemu <you@example.com>
2024-01-11 12:53:52 -0800

    uniqe code

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-from functools import reduce
-
 def uniq_add(my_list=[]):
-    new_list = reduce(lambda a, b: a + b, set(my_list))
-    return new_list
-
-# Example usage
-original_list = [1, 2, 3, 1, 2, 4, 5]
-result = uniq_add(my_list=original_list)
-print(result)
-
+    result = 0
+    for i in set(my_list):
+        result += i
+    return result
