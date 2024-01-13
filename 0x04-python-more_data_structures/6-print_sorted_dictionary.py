9035818d20db378300ed156fe3214570f07ee322
Yohannes Alemu <you@example.com>
2024-01-13 13:26:27 -0800

    john

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return (sorted(a_dictionary.key()))
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
+    print("{}: {}".format(k,a_dictionary[k]) for k in sorted(a_dictionary))
