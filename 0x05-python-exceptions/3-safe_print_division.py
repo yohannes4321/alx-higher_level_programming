9553eeb332b2a6adf252d5b1e535059a7919c342
Yohannes Alemu <you@example.com>
2024-01-23 11:46:05 -0800

    0x05-python-exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def safe_print_division(a, b):
     """
     divides two integers and prints the result
@@ -12,4 +11,4 @@ def safe_print_division(a, b):
         res = None
     finally:
         print("Inside result: {}".format(res))
-    return res
+        return res
