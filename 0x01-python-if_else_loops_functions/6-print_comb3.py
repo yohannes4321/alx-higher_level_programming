61b1667d6d24abbd02d517f9913b773c1828ef36
Yohannes Alemu <you@example.com>
2024-02-15 10:12:26 -0800

    python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 for i in range(0, 10):
-    for j in range(i+1, 10):
-        if i == 8 and j == 9:
-            print(("{}{}").format(i, j))
-        else:
-            print("{}{}".format(i, j), end=', ')
+    for j in range(1, 10):
+        if (i * 10 + j) == 89:
+            print("{:02d}".format(i * 10 + j))
+        elif (i * 10 + j) < (j * 10 + i):
+            print("{:02d}".format(i * 10 + j), end=', ')
