50152091a479fc122b39e8b95c2452f95a4f12f1
Yohannes Alemu <you@example.com>
2024-02-15 07:46:30 -0800

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if i == ord('q') or i == ord('e'):
-        continue
-    else:
-        print("{:c}".format(i), end='')
+for i in range(ord("a"), ord("z")+1):
+    if i != ord("q") and i != ord("e"):
+        print(chr(i))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
+        print(i)
+    else :
+        print("{}.format(if i <10 0 +str() ,else i >10 :)")
