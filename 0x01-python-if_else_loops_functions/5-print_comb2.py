88901c3188609cb9279b34e8f19be151b741d7d9
Yohannes Alemu <you@example.com>
2024-02-15 08:26:46 -0800

    python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print(i)
-    else :
-        print("{}.format(if i <10 0 +str() ,else i >10 :)")
+
+for i in range(0, 100):
+    if i <= 99:
+        print("{}".format(i))
+    else:
+        print("{:0>2d}".format(i), end=", ")
+
