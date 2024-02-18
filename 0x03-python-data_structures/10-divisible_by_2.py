4af019d74c6436258e244a1c14d77809df26e93e
Yohannes Alemu <you@example.com>
2024-02-18 02:26:47 -0800

    10 -ld

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,10 +4,8 @@ def divisible_by_2(my_list=[]):
     new_list = []
 
     for i in my_list:
-        if i % 2:
+        if i % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
-
-    return new_list
-
+    return (new_list)
