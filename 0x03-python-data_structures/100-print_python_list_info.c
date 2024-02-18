fb57b27cd4e066cfe17cf67e69b6f45d0676037e
Yohannes Alemu <you@example.com>
2024-02-18 03:12:46 -0800

    hello jesusu

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,56 +1,18 @@
-import ctypes
+#include <stdlib.h>
+#include <stdio.h>
+#include <Python.h>
+/**
+ * print_python_list_info -  function that prints some basic
+ *							info about Python lists
+ * @p: python list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int elem;
 
-lib = ctypes.CDLL('./libPyList.so')
-lib.print_python_list_info.argtypes = [ctypes.py_object]
-l = ['hello', 'World']
-lib.print_python_list_info(l)
-del l[1]
-lib.print_python_list_info(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
-lib.print_python_list_info(l)
-l = []
-lib.print_python_list_info(l)
-l.append(0)
-lib.print_python_list_info(l)
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
-lib.print_python_list_info(l)
-l.pop()
-lib.print_python_list_info(l)
-julien@ubuntu:~/CPython$ python3 100-test_lists.py 
-[*] Size of the Python List = 2
-[*] Allocated = 2
-Element 0: str
-Element 1: str
-[*] Size of the Python List = 1
-[*] Allocated = 2
-Element 0: str
-[*] Size of the Python List = 7
-[*] Allocated = 7
-Element 0: str
-Element 1: int
-Element 2: int
-Element 3: float
-Element 4: tuple
-Element 5: list
-Element 6: str
-[*] Size of the Python List = 0
-[*] Allocated = 0
-[*] Size of the Python List = 1
-[*] Allocated = 4
-Element 0: int
-[*] Size of the Python List = 5
-[*] Allocated = 8
-Element 0: int
-Element 1: int
-Element 2: int
-Element 3: int
-Element 4: int
-[*] Size of the Python List = 4
-[*] Allocated = 8
-Element 0: int
-Element 1: int
-Element 2: int
-Element 3: int
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (elem = 0; elem < Py_SIZE(p); elem++)
+		printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
+}
+
