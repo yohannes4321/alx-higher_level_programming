3080165197d3c6715725970b1dd14ec03b56103c
Yohannes Alemu <you@example.com>
2024-02-17 20:31:08 -0800

    2. Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,10 +2,9 @@
 
 def replace_in_list(my_list, idx, element):
     n = len(my_list) - 1
-
-    for i in my_list:
-        if i == idx:
-            my_list[i] = element
-            return my_list
-        elif idx < 0 or idx > n:
-            return my_list
+    if idx < 0:
+        return my_list
+    elif idx > n:
+        return my_list
+    my_list[idx] = element
+    return my_list
