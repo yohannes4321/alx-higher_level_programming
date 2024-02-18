8d11055350b9791627199a386350274a2ee854d3
Yohannes Alemu <you@example.com>
2024-02-18 00:37:05 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])
+    length = len(sentence)
+    first = sentence[0]
+    return length, first
