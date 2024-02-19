3ce962ef89733f392471572e6c0838cca20f7493
Yohannes Alemu <you@example.com>
2024-02-19 06:08:01 -0800

    hello jesus

---
+++ 0x04-python-more_data_structures/2_uni.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    
+    sum_of =0
+    for i in set(my_list):
+        sum_of +=i
+    return sum_of 
+
+
---
+++ 0x04-python-more_data_structures/5num.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    new_list=list(a_dictionary.keys())
+    k=len(new_list)
+    return k
+# to calculate the length dictonary  must be change to list 
