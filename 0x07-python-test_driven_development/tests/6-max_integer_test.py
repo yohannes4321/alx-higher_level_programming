e16741213d4884c8872dbe29b48be4a40f039540
Yohannes Alemu <you@example.com>
2024-02-28 01:53:06 -0800

    e

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python3
-"""Unittest for max_integer which returns the max int in a list.
+"""Unittest for max_integer([..])
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-    """A class that contains the test functions"""
+    """ Unit test for max_integer """
 
+    def test_positive(self):
+        """ test positive values """
+        self.assertEqual(max_integer([50, 100, 25]), 100)
 
-    def test_max(self):
-        """Test functionality of function"""
-        my_list = [1, 2, 3, 4]
-        result = max_integer(my_list)
-        self.assertEqual(result, 4)
-    def test_noelements(self):
-        """Test edge case where no elements exist"""
-        my_list = []
-        result = max_integer(my_list)
-        self.assertEqual(result, None)
+    def test_negative(self):
+        """ test negative values """
+        self.assertEqual(max_integer([-50, -100, -25]), -25)
+
+    def test_mix(self):
+        """ test mix values """
+        self.assertEqual(max_integer([50, -100, 25]), 50)
+
+    def test_empty_list(self):
+        """ test empty list """
+        self.assertEqual(max_integer([]), None)
+
+    def test_empty(self):
+        """ test empty"""
+        self.assertEqual(max_integer(), None)
+
+    def test_dictionary(self):
+        """ test dictionary """
+        self.assertRaises(KeyError, max_integer, {5: 'a', 4: 'b', 3: 'c'})
+
+    def test_one(self):
+        """ test one element in list """
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_string(self):
+        """ test string """
+        self.assertEqual(max_integer('hello'), 'o')
