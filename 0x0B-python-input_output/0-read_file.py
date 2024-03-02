fec1e0a5688296b54ec3e20be7456083f7c2ad53
Yohannes Alemu <you@example.com>
2024-03-02 09:07:40 -0800

    hs

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""crating a python file """
 def read_file(filename=""):
+    """read file function"""
     with open("filename", encoding="utf-8") as f:
         print(f.read())
