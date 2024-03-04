ac2539058cf2f4bda55a4c942c6e91cdbcf150af
Yohannes Alemu <you@example.com>
2024-03-04 09:39:06 -0800

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,7 +88,13 @@ class Rectangle(Base):
 
     def display(self):
         """display rectangle class"""
+        
         for i in range(self.height):
             for j in range(self.width):
                 print('#', end='')
             print()
+    def __str__(self):
+        string="[" + str(__class__.__name__) + "] " 
+        string +="("+str(self.id)+") " +str(self.x) + "/" + str(self.y) +" - "
+        string +=str(self.width) + "/" + str(self.height)
+        return string
