079225f8fadc408a8eacf5c4b86106c18a606903
Yohannes Alemu <you@example.com>
2024-03-04 11:25:58 -0800

    *args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -103,3 +103,16 @@ class Rectangle(Base):
         string += "/" + str(self.y) + " - "
         string += str(self.width) + "/" + str(self.height)
         return string
+
+    def update(self, *args):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
