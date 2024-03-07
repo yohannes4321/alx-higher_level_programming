e71674b68205980d9936405c2f2aa3271be4389c
Yohannes Alemu <you@example.com>
2024-03-07 07:27:45 -0800

    hello

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
+"""Defines a square class."""
 from models.rectangle import Rectangle
-"""Importing Rectangle"""
 
 
 class Square(Rectangle):
+    """Represent a square."""
+
     def __init__(self, size, x=0, y=0, id=None):
-        """INHERITANCE from Rectangle; there """
-        """is no width and height in Square"""
-        """super(). class must be replaced by size"""
-        """self.size = size"""
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
         super().__init__(size, size, x, y, id)
-        """k"""
-        self.size = size
-    """__str__"""
+
     def __str__(self):
-        """str"""
-        return f"[Square] ({self.id}) {self.x} / {self.y} - {self.size}"
+        """Return the print() and str() representation of a Square."""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
