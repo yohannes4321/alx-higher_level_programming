c86e95b8d7298473f4fbf840a62e33f5543fbc17
Yohannes Alemu <you@example.com>
2024-03-14 04:50:14 -0700

    Create TABLES

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,3 @@
 --script that creates a table called first_table
---first_table description:
---id INT
---name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS first_table
-(id INT,
-name VARCHAR(256)
-);
+--does not fail in table exists
+CREATE TABLE IF NOT EXISTS first_table(id INT , name VARCHAR(256));
