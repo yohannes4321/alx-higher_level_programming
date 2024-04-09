8c73401568e7fad701e4cdef87737793e894bbd1
Yohannes Alemu <alemuyohannes960@gmail.com>
2024-04-09 05:32:32 +0300

    j

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-// searches the second biggest integer in the list of arguments.
 
-if (process.argv.length <= 3) {
+if (process.argv.length <= 2) {
+  console.log(0);
+} else if (process.argv.length === 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  let numbers = process.argv.slice(2).map(Number);
+  let max = Math.max.apply(null, numbers);
+  numbers = numbers.filter(num => num !== max);
+  console.log(Math.max.apply(null, numbers));
 }
