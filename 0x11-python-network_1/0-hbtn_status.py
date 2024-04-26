45ac1aef53e252b079a7f5de22b1ea5e5632fb28
Yohannes Alemu <alemuyohannes960@gmail.com>
2024-04-26 23:35:19 +0300

    urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,14 @@
 """Fetch url usaing urllib"""
 from urllib import request
 
-url = "https://alx-intranet.hbtn.io/status"
-with request.urlopen(url) as response:
-    type = response.getheader("Content-Type")
-    content = response.read()
-    utf8 = content.decode("utf-8")
-    print("Body response:")
-    print("\t- type: " + type)
-    print("\t- content: " + content)
-    print("\t- utf content: " + utf8)
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with request.urlopen(url) as response:
+        type = response.getheader("Content-Type")
+        content = response.read()
+        utf8 = content.decode("utf-8")
+        print("Body response:")
+        print("\t- type: " + type)
+        print("\t- content: " + content)
+        print("\t- utf content: " + utf8)
 
