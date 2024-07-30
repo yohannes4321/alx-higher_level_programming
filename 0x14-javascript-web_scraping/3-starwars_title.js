86e2b675afdf79e061c83f9d9c6c7ce1c4112b0c
yohannes4321 <alemuyohannes960@gmail.com>
2024-07-30 15:16:56 +0300

    json parse

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
- 
 
-value1=process.argv[2];
-url='https://swapi-api.alx-tools.com/api/films/'
-const request=require('request')
-const url_final=url+value1
-request(url_final,(err,response,body)=>{
-    if (err){
-        console.err(err)
+const value1 = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const request = require('request');
+const urlFinal = url + value1;
 
-    }
-     
-    console.log(JSON.parse(body).title)
-
+request(urlFinal, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
