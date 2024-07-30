e49e28914b02376694625acfc497a5281f8fa8e6
yohannes4321 <alemuyohannes960@gmail.com>
2024-07-30 12:27:11 +0300

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
-// read file
-const fs=require('fs')
-const value=process.argv[2]
-fs.readFile(value,'utf8',(err,data)=>{
-    if (err){
-        console.error('Error:',err)
-    }
-    console.log(data)
-})
+
+// Import the 'fs' module
+const fs = require('fs');
+
+// Get the filename from the command-line arguments
+const value = process.argv[2];
+
+// Read the file asynchronously
+fs.readFile(value, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error:', err);
+    process.exit(1);
+  }
+  console.log(data);
+});
