1657795e2d6277244386d7a35db4441659aff631
yohannes4321 <alemuyohannes960@gmail.com>
2024-07-30 12:41:47 +0300

    0x14-javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Import the 'fs' module
+const fs = require('fs');
+
+// Get the filename and content from the command-line arguments
+const filename = process.argv[2];
+const content = process.argv[3];
+
+// Write the content to the specified file
+fs.writeFile(filename, content, 'utf8', (err) => {
+  if (err) {
+    console.error('Error:', err);
+    process.exit(1);
+  }
+  console.log('File written successfully');
+});
