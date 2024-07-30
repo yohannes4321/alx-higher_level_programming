b334aece3ced6f04553baa17b64feb4d6736a753
yohannes4321 <alemuyohannes960@gmail.com>
2024-07-30 12:46:34 +0300

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -8,7 +8,7 @@ const filename = process.argv[2];
 const content = process.argv[3];
 
 // Write the content to the specified file
-fs.writeFile(filename, content, 'utf8', (err,data) => {
+fs.writeFile(filename, content, 'utf8', (err, data) => {
   if (err) {
     console.error('Error:', err);
     process.exit(1);
